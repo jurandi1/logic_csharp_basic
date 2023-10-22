210afdd08739de2dd7605d3ec87fcfe49cdfeb82
Jurandi Junior <jurandijr20@gmail.com>
2023-10-21 21:24:28 -0300

    tipos de dados em C#

--- Course/Program.cs
+++ Course/Program.cs
@@ -7,7 +7,53 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Iniciando o curso de lógica de progrmação em C#");
+            //-------------------------------------------------------------------------------------------------
+            // AULA DE TIPOS DE DADOS EM C# - VARIAVEIS VALORES NUMÉRICOS
+            Console.WriteLine("-------------------------------------------------------------------------------------------------");
+            Console.WriteLine("AULA DE TIPOS DE DADOS EM C# - VARIAVEIS VALORES NUMÉRICOS");
+            Console.WriteLine("");
+
+            int int1 = int.MinValue;
+            int int2 = int.MaxValue;
+            sbyte sByte3 = sbyte.MinValue;
+            sbyte sByte4 = sbyte.MaxValue;
+            decimal dec5 = decimal.MaxValue;
+
+            Console.WriteLine(int1);
+            Console.WriteLine(int2);
+            Console.WriteLine(sByte3);
+            Console.WriteLine(sByte4);
+            Console.WriteLine(dec5);
+            Console.WriteLine("");
+
+            bool completo = false;
+            char genero = 'F';
+            char letra = '\u004D';
+            byte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648L;
+            float n5 = 4.5F;
+            double n6 = 4.5;
+            string nome = "Junior Green";
+            object obj1 = "Alex Brown";
+            object obj2 = 4.5F;
+
+            Console.WriteLine(completo);
+            Console.WriteLine(genero);
+            Console.WriteLine(letra);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
+            sbyte x = 100;
+
+            Console.WriteLine(x);
         }
     }
 }
