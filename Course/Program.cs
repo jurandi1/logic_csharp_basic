1a7badfbb9813e6c1f3a65ee96f02e2af6482146
Jurandi Junior <jurandijr20@gmail.com>
2024-02-10 19:59:11 -0300

    Operadores lógicos em C#

--- Course/Program.cs
+++ Course/Program.cs
@@ -8,32 +8,53 @@ namespace Course
         static void Main(string[] args)
         {
             //-------------------------------------------------------------------------------------------------
-            //AULA OPERADORES COMPARATIVOS EM C#
+            //AULA OPERADORES LÓGICOS EM C#
             Console.WriteLine("-------------------------------------------------------------------------------------------------");
-            Console.WriteLine("AULA OPERADORES COMPARATIVOS EM C#");
+            Console.WriteLine("AULA OPERADORES LÓGICOS EM C#");
             Console.WriteLine("");
 
-            int a = 10;
-            bool c1 = a < 10;
-            bool c2 = a < 20;
-            bool c3 = a > 10;
-            bool c4 = a > 5;
+            bool c1 = 2 > 3 && 4 != 5;
+            bool c2 = 2 > 3 || 4 != 5;
+            bool c3 = !(2 > 3) &&  4 != 5;
 
             Console.WriteLine(c1);
             Console.WriteLine(c2);
             Console.WriteLine(c3);
+            Console.WriteLine("-------------------------");
+
+            bool c4 = 10 < 5;
+            bool c5 = c2 || c2 && c3;
+
             Console.WriteLine(c4);
-            Console.WriteLine("----------------------------");
-
-            bool c5 = a <= 10;
-            bool c6 = a >= 10;
-            bool c7 = a == 10;
-            bool c8 = a != 10;
-
             Console.WriteLine(c5);
-            Console.WriteLine(c6);
-            Console.WriteLine(c7);
-            Console.WriteLine(c8);
+
+            //-------------------------------------------------------------------------------------------------
+            //AULA OPERADORES COMPARATIVOS EM C#
+            //Console.WriteLine("-------------------------------------------------------------------------------------------------");
+            //Console.WriteLine("AULA OPERADORES COMPARATIVOS EM C#");
+            //Console.WriteLine("");
+
+            //int a = 10;
+            //bool c1 = a < 10;
+            //bool c2 = a < 20;
+            //bool c3 = a > 10;
+            //bool c4 = a > 5;
+
+            //Console.WriteLine(c1);
+            //Console.WriteLine(c2);
+            //Console.WriteLine(c3);
+            //Console.WriteLine(c4);
+            //Console.WriteLine("----------------------------");
+
+            //bool c5 = a <= 10;
+            //bool c6 = a >= 10;
+            //bool c7 = a == 10;
+            //bool c8 = a != 10;
+
+            //Console.WriteLine(c5);
+            //Console.WriteLine(c6);
+            //Console.WriteLine(c7);
+            //Console.WriteLine(c8);
             ////-------------------------------------------------------------------------------------------------
             ////AULA DE ENTRADA DE DADOS EM C#
             //Console.WriteLine("-------------------------------------------------------------------------------------------------");
